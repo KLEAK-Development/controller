c369f0757a7252d6d8479ad4cebd6ac781e4295f
kevin <kevin@sakemaer.fr>
2024-07-24 09:25:07 +0200

    first commit

---
+++ example/lib/main.dart
@@ -0,0 +1,85 @@
+import 'package:controller/controller.dart';
+import 'package:flutter/material.dart';
+
+class CounterController extends Controller {
+  int _count;
+
+  CounterController({final int initialValue = 0}) : _count = initialValue;
+
+  int get count => _count;
+
+  void increment() {
+    _count++;
+    notifyListeners();
+  }
+}
+
+class CountryController extends Controller {
+  String _country;
+
+  CountryController({final String country = 'France'}) : _country = country;
+
+  String get country => _country;
+}
+
+void main() {
+  runApp(const App());
+}
+
+class Injection extends StatefulWidget {
+  final Widget child;
+
+  const Injection({super.key, required this.child});
+
+  @override
+  State<Injection> createState() => _InjectionState();
+}
+
+class _InjectionState extends State<Injection> {
+  final counterController = CounterController();
+
+  @override
+  Widget build(BuildContext context) {
+    return ControllerProvider(
+      controller: counterController,
+      child: widget.child,
+    );
+  }
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Injection(
+      child: MaterialApp(
+        home: Home(),
+      ),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: ControllerListenable<CounterController>(
+          builder: (context, controller) {
+            return Text('You pressed the button ${controller.count}');
+          },
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () {
+          final counterController = context.ofType<CounterController>();
+          counterController.increment();
+        },
+      ),
+    );
+  }
+}
---
+++ lib/controller.dart
@@ -0,0 +1,92 @@
+import "package:flutter/material.dart";
+
+abstract class Controller extends ChangeNotifier {
+  static T ofType<T extends Controller>(BuildContext context) {
+    return context
+        .dependOnInheritedWidgetOfExactType<_ControllerModel<T>>()!
+        .controller;
+  }
+
+  static T? maybeOfType<T extends Controller>(BuildContext context) {
+    return context
+        .dependOnInheritedWidgetOfExactType<_ControllerModel<T>>()
+        ?.controller;
+  }
+}
+
+class _ControllerModel<T> extends InheritedWidget {
+  const _ControllerModel({
+    super.key,
+    required super.child,
+    required this.controller,
+  });
+
+  final T controller;
+
+  @override
+  bool updateShouldNotify(_ControllerModel<T> oldWidget) {
+    return oldWidget.controller != controller;
+  }
+}
+
+class ControllerBuilder<T extends Controller> extends StatelessWidget {
+  final T controller;
+
+  final Widget Function(BuildContext context) builder;
+
+  const ControllerBuilder({
+    super.key,
+    required this.controller,
+    required this.builder,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return _ControllerModel<T>(
+      controller: controller,
+      child: ListenableBuilder(
+        listenable: controller,
+        builder: (context, _) => builder(context),
+      ),
+    );
+  }
+}
+
+class ControllerListenable<T extends Controller> extends StatelessWidget {
+  final Widget Function(BuildContext context, T controller) builder;
+
+  const ControllerListenable({super.key, required this.builder});
+
+  @override
+  Widget build(BuildContext context) {
+    final controller = Controller.ofType<T>(context);
+    return ListenableBuilder(
+      listenable: controller,
+      builder: (context, _) => builder(context, controller),
+    );
+  }
+}
+
+class ControllerProvider<T extends Controller> extends StatelessWidget {
+  const ControllerProvider({
+    super.key,
+    required this.controller,
+    required this.child,
+  });
+
+  final Widget child;
+  final T controller;
+
+  @override
+  Widget build(BuildContext context) {
+    return _ControllerModel<T>(controller: controller, child: this.child);
+  }
+}
+
+extension ControllerExtension on BuildContext {
+  T ofType<T extends Controller>() =>
+      getInheritedWidgetOfExactType<_ControllerModel<T>>()!.controller;
+
+  T? maybeOfType<T extends Controller>() =>
+      getInheritedWidgetOfExactType<_ControllerModel<T>>()?.controller;
+}
